ac048a3d5974acc9521a0e33a16cfe01d92d366e
abdullahzaf <67086785+abdullahzaf@users.noreply.github.com>
2020-06-21 15:49:50 -0500

    Update main.go

--- chatApplication/main.go
+++ chatApplication/main.go
@@ -3,7 +3,27 @@ import (
 	"log"
 	"net/http"
 )
+// temp1 represent a single template
+type templateHandler struct {
+	once		sync.once
+	filename	string
+	temp1		*template.Template
+}
+// ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 = template.Must (template.ParseFiles(filespath.Join("templates", t.filename)))
+}}
+t.temp1.Execute(w, nil)
+}
 func main() {
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	//start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
 			<html>
