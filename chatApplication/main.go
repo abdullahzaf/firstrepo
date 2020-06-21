ac9f59b220f224df0bee92be6644240179dbc081
abdullahzaf <67086785+abdullahzaf@users.noreply.github.com>
2020-06-21 18:36:30 -0500

    Update main.go

--- chatApplication/main.go
+++ chatApplication/main.go
@@ -37,6 +37,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse() //parse the flags
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	//get the room going
