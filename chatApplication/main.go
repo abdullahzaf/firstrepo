0dd7eee315c2c9e3813b61b553770531a5697ac7
abdullahzaf <67086785+abdullahzaf@users.noreply.github.com>
2020-06-21 21:33:06 -0500

    Update main.go

--- chatApplication/main.go
+++ chatApplication/main.go
@@ -7,18 +7,24 @@ import (
 	"github.com/stretchr/gomniauth/providers/github"
 	"github.com/stretchr/gomniauth/providers/google"
 )
-// temp1 represent a single template
+// templ represent a single template
 type templateHandler struct {
 	once		sync.once
 	filename	string
-	temp1		*template.Template
+	templ		*template.Template
 }
 // ServeHTTP handles the HTTP request
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.temp1 = template.Must(template.ParseFiles(filespath.Join("templates", t.filename)))
-}}
-t.temp1.Execute(w, r)
+		t.templ = template.Must(template.ParseFiles(filespath.Join("templates", t.filename)))
+})
+data := map[string]interface{}{
+	"Host": r.Host,
+}
+if authCookie, err := r.Cookie("auth"); err == nil {
+	data["UserData"] = objx.MustFromBase64(authCookie.Value)
+}
+t.templ.Execute(w, data)
 }
 func main() {
 
