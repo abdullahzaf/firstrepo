5090e35c1fb6d09a359fdd139857c866bb48269d
abdullahzaf <67086785+abdullahzaf@users.noreply.github.com>
2020-06-22 21:56:39 -0500

    Update auth.go

--- chatApplication/auth.go
+++ chatApplication/auth.go
@@ -1,30 +1,38 @@
 package main
 
-import(
-    "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
 )
+
 type authHandler struct {
-    next http.Handler
+	next http.Handler
 }
-func (h *authHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
-    _, err := r.Cookie("auth")
-    if err == http.ErrNoCookie {
-        //not authenticated
-        w.Header().Set("Location", "/login")
-        w.WriteHeader(http.StatusTemporaryRedirect)
-        return
-    }
-    if err != nil {
-        //some other error
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    //success - call the next Handler
-    h.next.ServeHTTP(w, r)
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("auth")
+	if err == http.ErrNoCookie {
+		//not authenticated
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+	if err != nil {
+		//some other error
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//success - call the next Handler
+	h.next.ServeHTTP(w, r)
 }
 func MustAuth(handler http.Handler) http.Handler {
-    return &authHandler{next: handler}
+	return &authHandler{next: handler}
 }
+
 //loginHandler handles the third-party login process format: /auth/{action}/{provider}
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	segs := strings.Split(r.URL.Path, "/")
@@ -39,10 +47,10 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
 		if err != nil {
-			http.Error(w, fmt.Sprtinf("Error when trying to GetBeginAuthURL for %s:%s", provider, err), http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("Error when trying to GetBeginAuthURL for %s:%s", provider, err), http.StatusInternalServerError)
 			return
 		}
-		w.Header.Set("Location", loginUrl)
+		w.Header().Set("Location", loginUrl)
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	case "callback":
 		provider, err := gomniauth.Provider(provider)
@@ -50,8 +58,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, fmt.Sprintf("Error when trying to get provider %s: %s", provider, err), http.StatusBadRequest)
 			return
 		}
-		creds, err := 
-	provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
 		if err != nil {
 			http.Error(w, fmt.Sprintf("Error when trying to complete auth for %s: %s", provider, err), http.StatusInternalServerError)
 			return
@@ -63,15 +70,13 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		authCookieValue := objx.New(map[string]interface{}{
 			"name": user.Name(),
-		}) .MustBase64()
+		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
-			Name: "auth",
+			Name:  "auth",
 			Value: authCookieValue,
-			Path: "/"})
+			Path:  "/"})
 		w.Header().Set("Location", "/chat")
 		w.WriteHeader(http.StatusTemporaryRedirect)
-		})
-		}
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "Auth action %s not supported", action)
