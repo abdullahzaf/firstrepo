0c49559323835e2411cc7eaf3774e38265540a3a
abdullahzaf <67086785+abdullahzaf@users.noreply.github.com>
2020-06-22 22:31:44 -0500

    Update main.go

--- chatApplication/main.go
+++ chatApplication/main.go
@@ -29,20 +29,9 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ.Execute(w, data)
 }
 func main() {
-
-	r := newRoom()
-	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
-	http.Handle("/login", &templateHandler{filename: "login.html"})
-	http.HandleFUnc("/auth/", loginHandler)
-	http.Handle("/room", r)
-	//get the room going
-	go r.run()
-	//start the web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse() //parse the flags
+
 	//setup gomniauth
 	gomniuath.SetSecurityKey("PUT YOUR AUTH KEY HERE")
 	gomniauth.WithProviders(
@@ -61,6 +50,7 @@ func main() {
 	http.Handle("/room", r)
 	//get the room going
 	go r.run()
+
 	//start the web server
 	log.Println("Starting web server on", *addr)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
