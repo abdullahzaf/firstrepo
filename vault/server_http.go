e191d9d37fd073476732d9d3ece8072dfc915fed
abdullahzaf <67086785+abdullahzaf@users.noreply.github.com>
2020-06-23 19:24:35 -0500

    Update server_http.go

--- vault/server_http.go
+++ vault/server_http.go
@@ -1,1 +1,12 @@
-go
+package vault
+import (
+    "net/http"
+    httptransport "github.com/go-kit/kit/transport/http"
+    "golang.org/x/net/context"
+)
+func NewHTTPServer(ctx context.Context, endpoints Endpoints) http.Handler {
+    m := http.NewServeMux()
+    m.Handle("/hash", httptransport.NewServer(ctx, endpoints.HashEndpoint, decodeHashRequest, encodeResponse,))
+    m.Handle("/validate", httptransport.NewServer(ctx, endpoints.ValidateEndpoint, decodeValidateRequest, encodeResponse,))
+        return m
+}
