66331c060c5799dd3ec54a75f9356c0de35a798d
abdullahzaf <67086785+abdullahzaf@users.noreply.github.com>
2020-06-23 19:22:34 -0500

    Update main_test.go

--- vault/cmd/vaultcli/main_test.go
+++ vault/cmd/vaultcli/main_test.go
@@ -1,1 +1,20 @@
-main_test.go
+func TestPop(t *testing.T) {
+    args := []string{"one", "two", "three"}
+    var s string
+    s, args = pop(args)
+    if s != "one" {
+        t.Errorf("unexpected "%s"", s)
+    }
+    s, args = pop(args)
+    if s != "two" {
+        t.Errorf("unexpected "%s"", s)
+    }
+    s, args = pop(args)
+    if s != "three" {
+        t.Errorf("unexpected "%s"", s)
+    }
+    s, args = pop(args)
+    if s != "" {
+        t.Errorf("unexpected "%s"", s)
+    }
+} 
