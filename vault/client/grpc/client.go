19aee07cf0b8d32fd2320e08c47943accf7a59e4
abdullahzaf <67086785+abdullahzaf@users.noreply.github.com>
2020-06-23 19:23:20 -0500

    Update client.go

--- vault/client/grpc/client.go
+++ vault/client/grpc/client.go
@@ -1,1 +1,27 @@
-client.go
+package grpc
+
+import(
+	grpctransport "github.com/go-kit/kit/transport/grpc"
+	"github.com/abdullahzaf/goRepo/vault"
+	"github.com/abdullahzaf/goRepo/vault/pb"
+	"google.golang.org/grpc"
+)
+
+func New(conn *grpc.ClientConn) vault.Service {
+    var hashEndpoint = grpctransport.NewClient(
+        conn, "Vault", "Hash",
+        vault.EncodeGRPCHashRequest,
+        vault.DecodeGRPCHashResponse,
+        pb.HashResponse{},
+    ).Endpoint()
+    var validateEndpoint = grpctransport.NewClient(
+        conn, "Vault", "Validate",
+        vault.EncodeGRPCValidateRequest,
+        vault.DecodeGRPCValidateResponse,
+        pb.ValidateResponse{},
+    ).Endpoint()
+    return vault.Endpoints{
+        HashEndpoint: hashEndpoint,
+        ValidateEndpoint: validateEndpoint,
+    }
+}
